be1427e04581a908cabac7b8a35981cd1c5c1fb4
viniciusventura29 <viniciusventura29@gmail.com>
2022-11-04 10:44:51 -0300

    adding dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,8 @@ export default function Home({ notes }: Notes) {
   });
   const router = useRouter();
 
+  const [darkMode, setDarkMode] = useState(false);
+
   const refreshData = () => {
     router.replace(router.asPath);
   };
@@ -38,7 +40,7 @@ export default function Home({ notes }: Notes) {
         fetch("/api/create", {
           body: JSON.stringify(data),
           headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
           },
           method: "POST",
         }).then(() => {
@@ -81,65 +83,70 @@ export default function Home({ notes }: Notes) {
   };
 
   return (
-    <div className="">
-      <h1 className="text-center font-bold text-2xl mt-4">Notes</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(form);
-        }}
-        className="w-auto mt-6 min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch"
-      >
-        <input
-          type="text"
-          placeholder="Title"
-          value={form.title}
-          onChange={(e) => setForm({ ...form, title: e.target.value })}
-          className="border-2 rounded border-gray-600 p-1"
-        />
-        <textarea
-          placeholder="Content"
-          value={form.content}
-          onChange={(e) => setForm({ ...form, content: e.target.value })}
-          className="resize-none h-28 border-2 rounded border-gray-600 p-1"
-        />
+    <div className={` ${darkMode && "dark"}`}>
+      <div className="min-h-screen bg-gray-100 dark:bg-slate-900">
+        <input type="checkbox" onClick={() => setDarkMode(!darkMode)}></input>
+        <h1 className="text-center dark:text-white font-bold text-2xl mt-4">
+          Notes
+        </h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(form);
+          }}
+          className="w-auto mt-6 min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch"
+        >
+          <input
+            type="text"
+            placeholder="Title"
+            value={form.title}
+            onChange={(e) => setForm({ ...form, title: e.target.value })}
+            className="border-2 rounded border-gray-600 p-2 border-gray-500 dark:text-gray-200 dark:bg-slate-800"
+          />
+          <textarea
+            placeholder="Content"
+            value={form.content}
+            onChange={(e) => setForm({ ...form, content: e.target.value })}
+            className="resize-none h-28 border-2 rounded border-gray-600 p-2 border-gray-500 dark:text-gray-200 dark:bg-slate-800"
+          />
 
-        <button type="submit" className="bg-blue-500 text-white rounded p-1">
-          Add +
-        </button>
-      </form>
+          <button type="submit" className="bg-blue-500 text-white rounded p-1">
+            Add +
+          </button>
+        </form>
 
-      <div className="w-auto min-w-[25%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch">
-        <ul>
-          {notes.map((note) => (
-            <li key={note.id} className="border-b border-gray-600 p-2">
-              <div className="flex justify-between">
-                <div className="flex-1">
-                  <h3 className="font-bold">{note.title}</h3>
-                  <p className="text-sm">{note.content}</p>
+        <div className="w-auto min-w-[25%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch dark:text-gray-200">
+          <ul>
+            {notes.map((note) => (
+              <li key={note.id} className="border-b border-gray-600 p-2">
+                <div className="flex justify-between">
+                  <div className="flex-1">
+                    <h3 className="font-bold">{note.title}</h3>
+                    <p className="text-sm">{note.content}</p>
+                  </div>
+                  <button
+                    onClick={() =>
+                      setForm({
+                        title: note.title,
+                        content: note.content,
+                        id: note.id,
+                      })
+                    }
+                    className="bg-blue-500 mr-3 px-3 text-white rounded"
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => deleteNote(note.id)}
+                    className="bg-red-500 px-3 text-white rounded"
+                  >
+                    X
+                  </button>
                 </div>
-                <button
-                  onClick={() =>
-                    setForm({
-                      title: note.title,
-                      content: note.content,
-                      id: note.id,
-                    })
-                  }
-                  className="bg-blue-500 mr-3 px-3 text-white rounded"
-                >
-                  Update
-                </button>
-                <button
-                  onClick={() => deleteNote(note.id)}
-                  className="bg-red-500 px-3 text-white rounded"
-                >
-                  X
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
