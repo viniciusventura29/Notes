afe197d075c92c91ecc99f39803261bd5ee664e6
viniciusventura29 <viniciusventura29@gmail.com>
2022-11-09 20:18:55 -0300

    📦 NEW: See more on cards

--- components/card.tsx
+++ components/card.tsx
@@ -12,47 +12,65 @@ export interface cardProps {
   setForm: Dispatch<SetStateAction<FormData>>;
 }
 
-
-
 export function Card({ note, setForm, deleteNote }: cardProps) {
-  const [modalComponent, setModalComponent] = useState(false)
+  const [modalComponent, setModalComponent] = useState(false);
+  let thereMore = ''
 
-  function callModal(){
-    if (modalComponent === true){
-      return <Modal note={note} />
+  function verifyLengthContent() {
+    if (note.content.length > 63){
+      thereMore = 'See More...'
     }else{
       null
     }
-    
+    console.log(thereMore)
+  }
+
+  function callModal() {
+    if (modalComponent === true) {
+      return <Modal note={note} />;
+    } else {
+      null;
+    }
   }
 
   return (
-    <li key={note.id} className="border-b border-gray-600 p-2">
-      <div onClick={() =>setModalComponent(!modalComponent)} className="cursor-pointer flex justify-between">
-        <div className="flex-1 h-12 overflow-hidden">
-          <h3 className="font-bold">{note.title}</h3>
-          <p className="text-sm pr-6">{note.content}</p>
+    <>
+    {verifyLengthContent()}
+      <li key={note.id} className="border-b border-gray-600 p-2">
+        <div
+          onClick={() => setModalComponent(!modalComponent)}
+          className="cursor-pointer flex justify-between"
+        >
+          <div className="flex-1 h-12 overflow-hidden">
+            <h3 className="font-bold">{note.title}</h3>
+            <div className="flex">
+            <p className="text-sm">
+              {note.content}
+            </p>
+            <p className="w-10 lg:w-40 text-sm italic font-semibold lg:ml-10">{thereMore}</p>
+            </div>
+          </div>
+          <button
+            onClick={() =>
+              setForm({
+                title: note.title,
+                content: note.content,
+                id: note.id,
+              })
+            }
+            className="bg-blue-500 hover:bg-blue-600 duration-500 mr-3 px-3 text-white rounded"
+          >
+            Update
+          </button>
+          <button
+            onClick={() => deleteNote(note.id)}
+            className="bg-red-500 hover:bg-red-600 duration-500 px-3 text-white rounded"
+          >
+            X
+          </button>
+          {callModal()}
         </div>
-        <button
-          onClick={() =>
-            setForm({
-              title: note.title,
-              content: note.content,
-              id: note.id,
-            })
-          }
-          className="bg-blue-500 hover:bg-blue-600 duration-500 mr-3 px-3 text-white rounded"
-        >
-          Update
-        </button>
-        <button
-          onClick={() => deleteNote(note.id)}
-          className="bg-red-500 hover:bg-red-600 duration-500 px-3 text-white rounded"
-        >
-          X
-        </button>
-        {callModal()}
-      </div>
-    </li>
+      </li>
+    </>
   );
 }
