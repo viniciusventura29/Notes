21b97ab0102c30fb52297f00d2a266d7231ae3da
viniciusventura29 <viniciusventura29@gmail.com>
2023-08-31 00:05:20 -0300

    fix node version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
-import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
