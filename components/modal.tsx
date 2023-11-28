18e6e9327822fa334b9c315e6c8a6f7fd4b95ebe
viniciusventura29 <viniciusventura29@gmail.com>
2023-11-28 11:48:07 -0300

    click outside

--- components/modal.tsx
+++ components/modal.tsx
@@ -9,7 +9,7 @@ export interface modalProps {
 export function Modal({ note, setModalComponent,modalComponent }: modalProps) {
   return (
     <div className={`flex justify-center items-center fixed h-screen w-full top-0 ${modalComponent? "":"hidden"}`}>
-      <div className="w-full fixed top-0 left-0 backdrop-blur-[3px] backdrop-brightness-90 h-full"></div>
+      <div onClick={()=>{setModalComponent(false)}} className="w-full fixed top-0 left-0 backdrop-blur-[3px] backdrop-brightness-90 h-full"></div>
       <div className="divide-solid divide-y dark:divide-slate-900 shadow-2xl bg-gray-50 dark:bg-slate-800 px-8 py-6 z-10 rounded-lg w-[50rem]">
         <div className="flex justify-between font-bold text-2xl break-words">
           <h1 className="dark:text-white" style={{overflowWrap:"anywhere"}}>{note?.title}</h1>
