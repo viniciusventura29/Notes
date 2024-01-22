9aaaaa6460ded17e473c7d51863f1c8e61f99479
viniciusventura29 <viniciusventura29@gmail.com>
2024-01-22 13:19:38 -0300

    new menu button

--- components/FilesView.tsx
+++ components/FilesView.tsx
@@ -39,18 +39,18 @@ export function FilesView({
   };
 
   return (
-    <div className="fixed bottom-10 right-14 flex flex-col-reverse gap-3 items-end">
+    <div className="flex flex-col-reverse gap-3 items-end">
       <button
         onClick={() => {
           setModalComponent(!modalComponent);
         }}
-        className="rounded-full bg-blue-500 transition-colors duration-200 hover:bg-blue-600 shadow border dark:border-slate-800 flex justify-center items-center p-4 text-blue-100 w-16 h-16"
+        className="rounded-full bg-blue-500 transition-colors duration-200 hover:bg-blue-600 shadow border dark:border-slate-800 flex justify-center items-center p-4 text-blue-100 w-14 h-14"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-files"
-          width="26"
-          height="26"
+          className="icon icon-tabler icon-tabler-folders"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
           strokeWidth="2"
           stroke="currentColor"
@@ -59,17 +59,16 @@ export function FilesView({
           strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M15 3v4a1 1 0 0 0 1 1h4" />
-          <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
-          <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
+          <path d="M9 4h3l2 2h5a2 2 0 0 1 2 2v7a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
+          <path d="M17 17v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2h2" />
         </svg>
       </button>
       <div
-        className={`rounded-md rounded-br-none border dark:border-gray-600 shadow dark:bg-slate-800 bg-slate-100 w-96 h-[32rem] overflow-y-scroll ${
+        className={`absolute left-16 bottom-24 rounded-md rounded-br-none border dark:border-gray-600 shadow dark:bg-slate-800 bg-slate-100 w-96 h-[32rem] overflow-y-scroll ${
           modalComponent ? "" : "hidden"
         }`}
       >
-        <h3 className="flex dark:text-white text-xl justify-center p-4 font-semibold">
+        <h3 className="dark:text-white text-xl justify-center p-4 font-semibold">
           Your Files
         </h3>
         {files?.map((file, index) => (
---
+++ components/MenuButton.tsx
@@ -0,0 +1,106 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import { FilesView } from "./FilesView";
+import { FileObject } from "../types";
+import { User } from "@supabase/supabase-js";
+
+export interface modalProps {
+  setModalComponent: Dispatch<SetStateAction<boolean>>;
+  modalComponent: boolean;
+  setNewNoteModalIsOpen: Dispatch<SetStateAction<boolean>>;
+  setNewFileModalIsOpen: Dispatch<SetStateAction<boolean>>;
+  files: FileObject[] | undefined
+  user: User | undefined
+}
+
+export function MenuButton({
+  setModalComponent,
+  modalComponent,
+  setNewNoteModalIsOpen,
+  setNewFileModalIsOpen,
+  files,
+  user
+}: modalProps) {
+  const [filesViewIsOpen, setFilesViewIsOpen] = useState<boolean>(false);
+
+  return (
+    <div className="fixed bottom-10 left-14 flex flex-col-reverse items-center gap-3">
+      <button
+        onClick={() => {
+          setModalComponent(!modalComponent);
+        }}
+        className="rounded-full bg-blue-500 transition-colors duration-200 hover:bg-blue-600 shadow border dark:border-slate-800 flex justify-center items-center p-4 text-blue-100 w-16 h-16"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-menu-2"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M4 6l16 0" />
+          <path d="M4 12l16 0" />
+          <path d="M4 18l16 0" />
+        </svg>
+      </button>
+      <div
+        className={`flex flex-col gap-4 ${modalComponent ? "" : "invisible"}`}
+      >
+        <button
+          onClick={() => setNewNoteModalIsOpen(true)}
+          className="w-14 h-14 p-4 bg-blue-500 rounded-full text-white hover:bg-blue-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-plus"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 5l0 14" />
+            <path d="M5 12l14 0" />
+          </svg>
+        </button>
+        <button
+          onClick={() => {
+            setNewFileModalIsOpen(true);
+          }}
+          className="w-14 h-14 p-4 bg-blue-500 rounded-full text-white hover:bg-blue-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-folder-plus"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 19h-7a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v3.5" />
+            <path d="M16 19h6" />
+            <path d="M19 16v6" />
+          </svg>
+        </button>
+        <button >
+        <FilesView files={files} modalComponent={filesViewIsOpen} setModalComponent={setFilesViewIsOpen} user={user} />
+        </button>
+        
+      </div>
+    </div>
+  );
+}
