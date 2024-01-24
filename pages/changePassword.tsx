06f5eabed63571603a2c36a8ea96b8672d7449e4
viniciusventura29 <viniciusventura29@gmail.com>
2024-01-24 13:47:06 -0300

    change password page and logic

--- pages/changePassword.tsx
+++ pages/changePassword.tsx
@@ -1,32 +1,48 @@
-import { useRouter } from "next/router";
 import { useState } from "react";
 import { useAlert } from "../components/Alert";
 import supabase from "./api/supabaseClient";
+import { useRouter } from "next/router";
 
-const Login = () => {
+const ChangePassword = () => {
   const router = useRouter();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [newPasswordConfirmation, setNewPasswordConfirmation] =
+    useState<string>("");
   const trigger = useAlert();
 
-  const login = () => {
-    supabase.auth
-      .signInWithPassword({ email: email, password: password })
+  const changePassword = async () => {
+    if (newPassword != newPasswordConfirmation) {
+      trigger({
+        text: "Your password confirmation is different of you new password",
+        title: "Your passwords are different",
+        type: "Error",
+      });
+      return;
+    }
+    const userEmail = router.query.userEmail;
+
+    if (!userEmail) return;
+    const users = await supabase.auth.admin.listUsers();
+    const singleUser = users.data.users.filter((u) => u.email == userEmail);
+    await supabase.auth.admin
+      .updateUserById(singleUser[0].id, {
+        password: newPassword,
+      })
       .then((e) => {
-        if(e.error){
+        if (e.data.user) {
           trigger({
-            text:e.error.message,
-            title:"Login failed",
-            type:"Error"
-          })
-          return
+            text: "Your password has been changed",
+            title: "Your password was successfully changed",
+            type: "Success",
+          });
+          router.push("/login");
+        } else {
+          trigger({
+            text: "An error happened",
+            title: "An unexpected error happened... Please, try again",
+            type: "Error",
+          });
         }
-        trigger({
-          text:"User Logged",
-          title:"Login Successfully",
-          type:"Success"
-        })
-        router.push("/");
       });
   };
 
@@ -35,70 +51,60 @@ const Login = () => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Login to your account
-            </h1>
+            <div className="flex flex-col gap-1">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Change your password
+              </h1>
+              <p className="text-gray-600 text-sm">
+                Write your new password below.
+              </p>
+            </div>
             <form
               className="space-y-4 md:space-y-6"
               onSubmit={(e) => {
-                login();
+                changePassword();
                 e.preventDefault();
               }}
             >
               <div>
                 <label
-                  htmlFor="email"
+                  htmlFor="newPassword"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your email
+                  New Password
                 </label>
                 <input
-                  type="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@email.com"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
                   type="password"
-                  placeholder="••••••••"
+                  id="newPassword"
+                  onChange={(e) => setNewPassword(e.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="********"
+                />
+                <label
+                  htmlFor="newPasswordConfirmation"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Repeat new Password
+                </label>
+                <input
+                  type="password"
+                  id="newPasswordConfirmation"
+                  onChange={(e) => setNewPasswordConfirmation(e.target.value)}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="********"
                 />
               </div>
 
               <button
                 type="submit"
                 onClick={(e) => {
-                  login();
+                  changePassword();
                   e.preventDefault();
                 }}
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Login
+                Change password
               </button>
-              <a
-                  href="/forgotPassword"
-                  className="text-sm text-blue-500 hover:underline dark:text-blue-500"
-                >
-                  Forgot the password?
-                </a>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
-                <a
-                  href="/register"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Sign up
-                </a>
-              </p>
             </form>
           </div>
         </div>
@@ -107,4 +113,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ChangePassword;
--- pages/forgotPassword.tsx
+++ pages/forgotPassword.tsx
@@ -3,31 +3,13 @@ import { useState } from "react";
 import { useAlert } from "../components/Alert";
 import supabase from "./api/supabaseClient";
 
-const Login = () => {
+const ForgotPassword = () => {
   const router = useRouter();
   const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const trigger = useAlert();
 
-  const login = () => {
-    supabase.auth
-      .signInWithPassword({ email: email, password: password })
-      .then((e) => {
-        if(e.error){
-          trigger({
-            text:e.error.message,
-            title:"Login failed",
-            type:"Error"
-          })
-          return
-        }
-        trigger({
-          text:"User Logged",
-          title:"Login Successfully",
-          type:"Success"
-        })
-        router.push("/");
-      });
+  const sendRecoveryPasswordEmail = async () => {
+    await supabase.auth.resetPasswordForEmail(email);
   };
 
   return (
@@ -35,13 +17,18 @@ const Login = () => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Login to your account
-            </h1>
+            <div className="flex flex-col gap-1">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Forgot your password
+              </h1>
+              <p className="text-gray-600 text-sm">
+                Write your e-mail below and we will send a recovery link.
+              </p>
+            </div>
             <form
               className="space-y-4 md:space-y-6"
               onSubmit={(e) => {
-                login();
+                sendRecoveryPasswordEmail();
                 e.preventDefault();
               }}
             >
@@ -59,37 +46,17 @@ const Login = () => {
                   placeholder="name@email.com"
                 />
               </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
-              </div>
 
               <button
                 type="submit"
                 onClick={(e) => {
-                  login();
+                  sendRecoveryPasswordEmail();
                   e.preventDefault();
                 }}
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Login
+                Send e-mail
               </button>
-              <a
-                  href="/forgotPassword"
-                  className="text-sm text-blue-500 hover:underline dark:text-blue-500"
-                >
-                  Forgot the password?
-                </a>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
                 <a
@@ -107,4 +74,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ForgotPassword;
